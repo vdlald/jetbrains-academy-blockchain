6d6bd08d59a812dda8c95e9c5ea5bb50955832dd
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-05-20 10:38:04 +0300

    init

---
+++ src/main/java/com/vladislav/blockchain/Sandbox.java
@@ -0,0 +1,12 @@
+package com.vladislav.blockchain;
+
+public class Sandbox implements Runnable {
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        new Sandbox().run();
+    }
+}
